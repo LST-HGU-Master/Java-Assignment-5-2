df7bfce06b939f397fc74f61878179f1290a6a28
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-09 21:50:54 +0900

    Update Prog52.java

--- src/main/java/Prog52.java
+++ src/main/java/Prog52.java
@@ -1,10 +1,10 @@
 public class Prog52 {
 
-	public static void main(String[] args) {
-		int a = Integer.parseInt(args[0]);
-		int b = Integer.parseInt(args[1]);
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
 
-	}
+    }
 
 }
 
